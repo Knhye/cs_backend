80222686da044262ae4bd7fb498c779eb7033b59
Knhye <redroses4701@gmail.com>
2026-06-02 19:58:56 +0900

    chore: 로그 확인을 위한 설정

--- src/common/utils/error.util.ts
+++ src/common/utils/error.util.ts
@@ -1,6 +1,9 @@
-import { HttpException, InternalServerErrorException } from '@nestjs/common';
+import { HttpException, InternalServerErrorException, Logger } from '@nestjs/common';
+
+const logger = new Logger('rethrowAsInternal');
 
 export function rethrowAsInternal(e: unknown, message: string): never {
   if (e instanceof HttpException) throw e;
+  logger.error(message, e instanceof Error ? e.stack : String(e));
   throw new InternalServerErrorException(message);
 }
