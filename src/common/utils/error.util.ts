3dc2ca1cad47229fd498c1f695ee2c5eb658469a
Knhye <redroses4701@gmail.com>
2026-06-02 20:02:44 +0900

    fix: ssl 설정 변경

--- src/common/utils/error.util.ts
+++ src/common/utils/error.util.ts
@@ -1,9 +1,6 @@
-import { HttpException, InternalServerErrorException, Logger } from '@nestjs/common';
-
-const logger = new Logger('rethrowAsInternal');
+import { HttpException, InternalServerErrorException } from '@nestjs/common';
 
 export function rethrowAsInternal(e: unknown, message: string): never {
   if (e instanceof HttpException) throw e;
-  logger.error(message, e instanceof Error ? e.stack : String(e));
   throw new InternalServerErrorException(message);
 }
