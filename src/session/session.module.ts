f9027f4022265c67e7587dfdcd2194cb27dd0ed6
Knhye <redroses4701@gmail.com>
2026-06-09 13:58:10 +0900

    feat: 배지 획득 시 알림 발송

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module.js';
 import { BadgeModule } from '../badge/badge.module.js';
+import { FcmModule } from '../fcm/fcm.module.js';
 import { SessionController } from './session.controller.js';
 import { SessionService } from './session.service.js';
 
 @Module({
-  imports: [AuthModule, BadgeModule],
+  imports: [AuthModule, BadgeModule, FcmModule],
   controllers: [SessionController],
   providers: [SessionService],
 })
